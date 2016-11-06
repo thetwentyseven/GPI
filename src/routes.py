e30a244ce4be4653d986a8851927320dc03ea4dc
thetwentyseven <adrian.vcch@gmail.com>
2016-11-06 19:37:27 +0000

    Creating 401, 404 templates, and working on login page

--- src/routes.py
+++ src/routes.py
@@ -1,12 +1,44 @@
 import ConfigParser
+import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, render_template, flash
 
 app = Flask(__name__)
 
+# Database
+conn = sqlite3.connect('database/gpi.db')
+c =  conn.cursor()
+# c.execute('SELECT * FROM users')
+# data = c.fetchall() #single fetchone()
+# print(data)
+# for row in c.fetchall():
+    # print(row)
+
+
 # Routes
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html')
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return render_template('401.html')
+
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method=='POST':
+        email = request.form['email']
+        password = request.form['password']
+        #c.execute('SELECT email, password FROM users')
+        #c.execute('SELECT email, password FROM users WHERE email=? AND password =?', email, password)
+        data = 0 #c.fetchone()
+        if data:
+            return render_template('login.html', data=data)
+    else:
+        return render_template('login.html')
 
 
 # Configuration
